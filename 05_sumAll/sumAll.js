dfdedaf0f8e7417e2e74165146c335cf9b1ebbf3
dreina99 <devinreina99@gmail.com>
2023-07-02 16:21:02 -0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0 || typeof start != "number" || typeof end != "number") 
+        return "ERROR"
+    
+    // ensure start is always less than end
+    let arr = [start, end]
+    arr.sort()
 
+    let sum = 0
+    for(let i = arr[0]; i <= arr[1]; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
