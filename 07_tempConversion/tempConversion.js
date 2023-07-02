c5c6c3287842090e0190e0cf104ddb7683f28298
dreina99 <devinreina99@gmail.com>
2023-07-02 16:33:30 -0700

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let cTemp = (fTemp - 32) / 1.8
+  return parseFloat(cTemp.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fTemp = (cTemp * 9/5) + 32
+  return parseFloat(fTemp.toFixed(1))
 };
 
 // Do not edit below this line
