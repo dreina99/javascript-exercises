18bb7c6b4921a279d4f57e0eb8929536aabcdac3
dreina99 <devinreina99@gmail.com>
2023-07-08 14:47:43 -0700

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str
+        .toLowerCase()
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+        .replaceAll(' ', '')
+    reverseStr = str.split('').reverse().join('')
+    return str === reverseStr
 };
 
 // Do not edit below this line
