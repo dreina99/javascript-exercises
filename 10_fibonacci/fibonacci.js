eda296835fb96a51a264ec58da2b5416457ac1a4
dreina99 <devinreina99@gmail.com>
2023-07-08 15:37:43 -0700

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(fibNum) {
+    fibNum = parseInt(fibNum)
+    if(fibNum <= 0)
+        return "OOPS"
+    else if(fibNum == 1 || fibNum == 2)
+        return 1
 
+    fibTotal = 1 + 1
+    prevFib = 1
+    for(let i = 0; i < fibNum - 3; i++) {
+        temp = fibTotal
+        fibTotal = fibTotal + prevFib
+        prevFib = temp
+    }
+    return fibTotal
 };
 
 // Do not edit below this line
