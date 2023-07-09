f8e94b65a225c10886f7f36c5e40ffb16ffc7243
dreina99 <devinreina99@gmail.com>
2023-07-08 17:39:04 -0700

    Finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    ages = []
+    arr.forEach(item => {
+        let currLifeSpan = 0
+        if(item.yearOfDeath == undefined)
+            currLifeSpan = new Date().getFullYear() - item.yearOfBirth
+        else 
+            currLifeSpan = item.yearOfDeath - item.yearOfBirth
+        item.yearsLived = currLifeSpan
+    })
 
+    arr.sort((a, b) => {
+        if(a.yearsLived > b.yearsLived) return -1
+        else return 1
+    })
+    
+    return arr[0]
 };
 
+const findTheOldest2 = function(arr) {
+    return arr.reduce((oldest, currPerson) => {
+        oldestYears = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        currYears = getAge(currPerson.yearOfBirth, currPerson.yearOfDeath)
+
+        return oldestYears < currYears ? currPerson : oldest
+    })
+}
+
+const getAge = (birth, death) => {
+    if(!death)
+        death = new Date().getFullYear()
+    return death - birth
+}
+
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest2;
